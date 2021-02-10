45d2cd48723b7ccdc2ad72f9621a4d561753bc7c
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2021-02-10 04:50:47 +0000

    added feature to check for user login and signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,6 +27,18 @@ class ApplicationController < ActionController::Base
       expires: 10.days.from_now
     }
     cookies[:cart]
+  end 
+
+  #allows us to check if a person is logged in
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+  helper_method :current_user
+
+  #can use 'before_filter :authorize' to secure controllers from not logged in clients
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+
 
 end
